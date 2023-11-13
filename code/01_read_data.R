055281a560f55ad1fbe254e800ce7756199e95e6
bpfree <brian.patrick.free@gmail.com>
2023-11-13 16:29:05 -0500

    Calculate distance in points and time differences

--- code/01_read_data.R
+++ code/01_read_data.R
@@ -1,6 +1,14 @@
 rm(list = ls())
 
 library(dplyr)
+library(marmap)
+library(sf)
+library(geosphere)
+
+# Resources
+## geosphere package Github: https://github.com/rspatial/geosphere
+## geosphere reference manual: https://cran.r-project.org/web/packages/geosphere/geosphere.pdf
+
 
 dir.create("data/zz_miscellaneous_data")
 
@@ -52,7 +60,78 @@ list(unique(shrimp_pings_2014$SERIAL))
 ########################
 
 vessel1 <- shrimp_pings_2014 %>%
-  dplyr::filter(VSBN == "1022072")
+  dplyr::select(VSBN,
+                LONGITUDE,
+                LATITUDE,
+                start_date,
+                start_time) %>%
+  # filter for test vessels
+  dplyr::filter(VSBN %in% c("1022072",
+                            "1023214",
+                            "1027077",
+                            "1027078")) %>%
+  # convert to sf feature
+  sf::st_as_sf(coords = c("LONGITUDE", "LATITUDE"),
+               # set the coordinate reference system to WGS84
+               crs = 4326, # EPSG 4326 (https://epsg.io/4326)
+               # keep longitude and latitude fields
+               remove = FALSE)
+
+list(unique(vessel1$VSBN))
+View(vessel1)
+
+# time change calculation
+vessel1_diff <- vessel1 %>%
+  # calculate distance between a point and the previous point
+  ## ***Note: geopackage was used originally by Kyle Dettloff (kyle.dettloff@noaa.gov) when creating the original dataset
+  ##          The methods noted that the previous analysis used Vincenty ellipsoid method given it took the curvature
+  ##          of the earth into consideration (https://sedarweb.org/documents/sedar-87-dw-01-estimation-of-commercial-shrimp-effort-in-the-gulf-of-mexico/)
+  ##          Original methods: https://github.com/kyledettloff-NOAA/GOMshrimpEffort/blob/main/effort_scaled.R
   
+  ## To learn more about the methods see here: http://www.movable-type.co.uk/scripts/latlong-vincenty.html
+  ## and here: https://github.com/rspatial/geosphere/blob/master/R/distVincentyEllipsoid.R
+  
+  ## ***Note: distances will need to be under 1 nautical mile (1 nautical mile = 1852 meters)
+  mutate(nm = geosphere::distVincentyEllipsoid(p1 = cbind(LONGITUDE, LATITUDE),
+                                               p2 = cbind(lag(LONGITUDE), lag(LATITUDE))) / 1852,
+         # calculate the time between 
+         mins_diff = as.numeric(STAMP - lag(STAMP), units = "mins")) %>%
+  dplyr::select(-LONGITUDE,
+                -LATITUDE)
+
+vess_a <- vessel1_diff %>%
+  dplyr::filter(VSBN == "1022072") %>%
+  dplyr::slice(1:10)
+
+vess_b <- vessel1_diff %>%
+  dplyr::filter(VSBN == "1023214") %>%
+  dplyr::slice(1:10)
+
+vess_c <- vessel1_diff %>%
+  dplyr::filter(VSBN == "1027077") %>%
+  dplyr::slice(1:10)
+
+vess_d <- vessel1_diff %>%
+  dplyr::filter(VSBN == "1027078") %>%
+  dplyr::slice(1:10)
+
+
+vess_comb <- rbind(vess_a,
+                   vess_b,
+                   vess_c,
+                   vess_d) %>%
+  dplyr::select(VSBN,
+                nm,
+                mins_diff) %>%
+  # use cumsum() to have transect go from 1 to 2 to ... (rolling calculation)
+  # use RcppRoll::roll_sum(v)) [https://itsalocke.com/blog/understanding-rolling-calculations-in-r/]
+  dplyr::mutate(transect_id = ifelse(
+    lead(VSBN) == VSBN &
+      lead(nm) <= 1.0 &
+      lead(mins_diff) <= 30,
+    paste0("transect_", row_number(), "_", lead(row_number())), NA))
+
+View(vess_comb)
+
 # Export data
 
